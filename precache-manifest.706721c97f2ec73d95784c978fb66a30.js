a498fb3b343811b66317acecc0fc8bf8b4b88c41
Max Greenwald <powerslab3@gmail.com>
2023-10-29 10:47:34 -0400

    Updates

---
+++ precache-manifest.706721c97f2ec73d95784c978fb66a30.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4158e425777fb0216a4b3394d5ca9679",
+    "url": "/ACH_Task_Short_Psychedelic3/index.html"
+  },
+  {
+    "revision": "88229d075a7f92d889c1",
+    "url": "/ACH_Task_Short_Psychedelic3/static/css/main.f79609ab.chunk.css"
+  },
+  {
+    "revision": "3e7b4221aa9d2a75b47f",
+    "url": "/ACH_Task_Short_Psychedelic3/static/js/2.01d8e118.chunk.js"
+  },
+  {
+    "revision": "ee45c4ac1e2cdbe20661beb901b1137f",
+    "url": "/ACH_Task_Short_Psychedelic3/static/js/2.01d8e118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88229d075a7f92d889c1",
+    "url": "/ACH_Task_Short_Psychedelic3/static/js/main.87787147.chunk.js"
+  },
+  {
+    "revision": "517178af888e218f6abc",
+    "url": "/ACH_Task_Short_Psychedelic3/static/js/runtime-main.de9b3fa9.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/ACH_Task_Short_Psychedelic3/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "d9fbf22f557b87c921a73ef996721520",
+    "url": "/ACH_Task_Short_Psychedelic3/static/media/rating_keydown_1.d9fbf22f.png"
+  },
+  {
+    "revision": "6a659dd41fa6c15739f611662bf6d8d1",
+    "url": "/ACH_Task_Short_Psychedelic3/static/media/rating_keydown_2.6a659dd4.png"
+  },
+  {
+    "revision": "d955b91a2b4ba7dca40ee5ccb82d995b",
+    "url": "/ACH_Task_Short_Psychedelic3/static/media/rating_keydown_3.d955b91a.png"
+  },
+  {
+    "revision": "c8b7c6860406752a1915e08a432cb320",
+    "url": "/ACH_Task_Short_Psychedelic3/static/media/rating_keydown_4.c8b7c686.png"
+  },
+  {
+    "revision": "34958048ce563b1140c19e19f6e72410",
+    "url": "/ACH_Task_Short_Psychedelic3/static/media/rating_keydown_5.34958048.png"
+  }
+]);
